9f86d7507ffd4afd87d72dc8a6d9e863cf8919b6
tonniegeetz <tonniegeetz@gmail.com>
2024-07-04 11:55:10 +0300

    associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,20 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    array = []
+    artists.map do |musician|
+      array << musician.name
+    end
+    array
   end
 end
